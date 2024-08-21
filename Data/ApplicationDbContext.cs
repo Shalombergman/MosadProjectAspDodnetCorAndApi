e8400c0c1d3ac1a2361e0065a134cece1c48f81e
shalom bergman <shalom195295@gmail.com>
2024-08-21 12:02:04 +0300

    Create ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MosadApiServer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
